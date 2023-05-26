9162b64240133675aaf544fb52702bd1c3354841
yo_boi <abdelrahmanbbb@gmail.com>
2023-05-27 01:29:37 +0300

    fixed a bug

--- Santa_clause_problem.c
+++ Santa_clause_problem.c
@@ -7,7 +7,7 @@
 #include <stdbool.h>
 
 const int DEER = 9;
-const int ELVES = 3;
+const int ELVES = 10;
 
 pthread_mutex_t santaLock; /* lock for access to santa */
 int santaDoor = 0; /* int that keeps track of elves waiting at Santa's door (only 3 allowed) */
@@ -18,6 +18,8 @@ sem_t lastElf; /* semaphore to make sure the last elf gets helped by santa and n
 
 int deerInStable = 0;
 sem_t deerWaiting;
+bool deerReady = false;
+sem_t deerFinished;
 pthread_mutex_t deerMutex; /* mutex to control access to the deer int */
 
 
@@ -32,6 +34,7 @@ void* elf(void* arg){
     printf("elf (%lu) is working for %d\n", pthread_self(), workDuration);
     sleep(workDuration); /* elf works for a random amount of time and then runs into a problem */
     pthread_mutex_lock(&santaLock);
+    printf("elf (%lu) has a problem and goes to Santa for help\n %d\n", pthread_self(),santaDoor);
     pthread_mutex_lock(&elfQueMutex);
     santaDoor++;
     if(santaDoor == 3){
@@ -55,12 +58,13 @@ void deliverPresents(){
   pthread_mutex_lock(&deerMutex);
   deerInStable--;
   pthread_mutex_unlock(&deerMutex);
+  sem_post(&deerFinished);
   printf("reindeer (%lu) helped deliver presents\n", pthread_self());
 }
 
 void* deer(void* arg){
   while(true){
-    int vacationTime = rand() % 20; /* take a vaccation for 0 to 19 seconds */
+    int vacationTime = rand() % 10; /* take a vaccation for 0 to 19 seconds */
     printf("reindeer (%lu) is vacationing for %d\n", pthread_self(), vacationTime);
     sleep(vacationTime);
     pthread_mutex_lock(&santaLock);
@@ -71,6 +75,7 @@ void* deer(void* arg){
       sem_post(&wakeSanta);
       pthread_mutex_unlock(&deerMutex);
       sem_wait(&deerWaiting);
+      while(!deerReady);
       deliverPresents();
       pthread_mutex_unlock(&santaLock);
     }else{
@@ -78,6 +83,7 @@ void* deer(void* arg){
       pthread_mutex_unlock(&santaLock);
       pthread_mutex_unlock(&deerMutex);
       sem_wait(&deerWaiting);
+      while(!deerReady);
       deliverPresents();
     }
   }
@@ -92,7 +98,13 @@ void* santa(void* arg){
 	sem_post(&deerWaiting);
 	printf("Santa fastened a deer to the sleigh\n");
       }
-      printf("Santa delivered the presents and returned\n");
+      printf("Santa fastened all the deer and will deliver the presents\n");
+      deerReady = true;
+      for(int i = 0; i < DEER; i++){
+	sem_wait(&deerFinished);
+      }
+      deerReady = false;
+      printf("Santa delivered all presents and returned to the north pole\n");
     }else if(santaDoor == 3){
 	sem_post(&helpQue);
 	printf("Santa helped an elf\n");
@@ -113,6 +125,7 @@ int main(){
   sem_init(&lastElf, 0, 0);
   pthread_mutex_init(&deerMutex, NULL);
   sem_init(&deerWaiting, 0, 0);
+  sem_init(&deerFinished, 0, 0);
 
   pthread_t deerThreads[DEER];
   for(int i = 0; i < DEER; i++){
@@ -128,7 +141,7 @@ int main(){
   }
 
   pthread_join(santaThread, NULL);
-
+  
   for(int i = 0; i < ELVES; i++){
     pthread_join(elfThreads[i], NULL);
   }
